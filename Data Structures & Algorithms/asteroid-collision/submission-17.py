b6c4818ed54b249d7ccbf0a15edc1d2202aba29b
filipmdespotovic <filip.m.despotovic@gmail.com>
2026-05-18 12:50:57 +0200

    Add: asteroid-collision - submission-17

---
+++ Data Structures & Algorithms/asteroid-collision/submission-17.py
@@ -0,0 +1,28 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        
+        stek = []
+
+        for asteroid in asteroids:
+            if not stek or asteroid > 0:
+                stek.append(asteroid)
+                continue
+            
+            if abs(asteroid) == stek[-1]:
+                stek.pop()
+                continue
+            
+            if stek[-1] < 0:
+                stek.append(asteroid)
+                continue
+
+            while stek and stek[-1] > 0 and abs(asteroid) > stek[-1]:
+                stek.pop()
+
+            if not stek or stek[-1] < 0:
+                stek.append(asteroid)
+            
+            if stek[-1] == abs(asteroid):
+                stek.pop()
+
+        return stek
